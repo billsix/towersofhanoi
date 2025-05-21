e0565d13d9ebd78c9b8072b4c9de6f8ad7bc863d
William Emerison Six <billsix@gmail.com>
2025-05-21 10:32:38 -0400

    updated

--- bash/pipe/hanoi4.sh
+++ bash/pipe/hanoi4.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# move a tower, sized 2, from tower 1 to tower 2
 ./hanoi3.sh | tr '123' '132'
+# move a tower, sized 1, from tower 1 to tower 3
 ./hanoi1.sh
+# move a tower, sized 3, from tower 2 to tower 3
 ./hanoi3.sh | tr '123' '213'
