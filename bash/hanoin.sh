f648c82864a4c898bb1b6ad261414b4cdd8787d4
William Emerison Six <billsix@gmail.com>
2025-07-23 00:01:03 -0400

    updated hanoin

--- bash/hanoin.sh
+++ bash/hanoin.sh
@@ -1,16 +1,14 @@
 #!/usr/bin/env bash
 
 n=$1
-thisScript=$0
-
 if [ "$n" -eq 1 ]; then
     ./hanoi1.sh
     exit
 fi
 
 # move a tower, sized n-1, from tower 1 to tower 2
-$thisScript $((n-1)) | tr '23' '32'
+./hanoin.sh $((n-1)) | tr '123' '132'
 # move a tower, sized 1, from tower 1 to tower 3
-$thisScript 1
+./hanoin.sh 1
 # move a tower, sized n-1, from tower 2 to tower 3
-$thisScript $((n-1)) | tr '12' '21'
+./hanoin.sh $((n-1)) | tr '123' '213'
