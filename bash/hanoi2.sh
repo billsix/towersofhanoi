a33d9e98b8ea2d38490bc207e37b43bc39cb4099
William Emerison Six <billsix@gmail.com>
2025-07-24 21:12:32 -0500

    made bash examples more clear

--- bash/hanoi2.sh
+++ bash/hanoi2.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 # move a tower, sized 1, from tower 1 to tower 2
-./hanoi1.sh | tr '23' '32'
+./hanoi1.sh | tr '123' '132'
 # move a tower, sized 1, from tower 1 to tower 3
 ./hanoi1.sh
 # move a tower, sized 1, from tower 2 to tower 3
-./hanoi1.sh | tr '12' '21'
+./hanoi1.sh | tr '123' '213'
--- bash/hanoi3.sh
+++ bash/hanoi3.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 # move a tower, sized 2, from tower 1 to tower 2
-./hanoi2.sh | tr '23' '32'
+./hanoi2.sh | tr '123' '132'
 # move a tower, sized 1, from tower 1 to tower 3
 ./hanoi1.sh
 # move a tower, sized 2, from tower 2 to tower 3
-./hanoi2.sh | tr '12' '21'
+./hanoi2.sh | tr '123' '213'
